1fe138a83d2c968cfc3c104c22ecf37369d72ccf
AntonKo24 <anton.korieniev.dev@gmail.com>
2023-09-01 11:32:54 +0300

    Update 1.02

--- app/src/main/java/com/tonyk/android/codapizza/MainActivity.kt
+++ app/src/main/java/com/tonyk/android/codapizza/MainActivity.kt
@@ -3,14 +3,15 @@ package com.tonyk.android.codapizza
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.tonyk.android.codapizza.ui.components.ToppingCell
+import com.tonyk.android.codapizza.ui.pizzaorder.PizzaBuilderScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ToppingCell()
+            PizzaBuilderScreen()
         }
     }
 }
